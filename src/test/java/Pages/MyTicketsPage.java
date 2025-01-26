b26a50ae622af38fa002c5568ab5a17dc47e47fb
yusuf <yusuffdemirci10@gmail.com>
2025-01-26 21:59:13 +0300

    FilterHelper and GamePageHelper classes integrated into page classes

--- src/test/java/Pages/MyTicketsPage.java
+++ src/test/java/Pages/MyTicketsPage.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import util.ElementHelper;
-import util.FilterHelper;
 
 import java.time.Duration;
 
@@ -15,9 +14,8 @@ import static util.Constants.EXPLICIT_WAIT;
 
 public class MyTicketsPage {
     WebDriver driver;
-    util.ElementHelper elementHelper;
+    ElementHelper elementHelper;
     WebDriverWait wait;
-    FilterHelper filterHelper;
 
     @FindBy(xpath = "//a[contains(text(),'Hesabım')]")
     WebElement accountButton;
@@ -28,12 +26,73 @@ public class MyTicketsPage {
     @FindBy(css = "div.MuiGrid-root.MuiGrid-container.css-1d3bbye > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.MuiGrid-grid-sm-6.css-1e6jl34-card_grid_container:last-of-type span.css-4qwqxz-footer_details_content")
     WebElement detailButton;
 
+    @FindBy(xpath = "//div[2]//div[1]//button[1]")
+    private WebElement milliPiyangoButton;
+
+    @FindBy(xpath = "//div[3]//div[1]//button[1]")
+    private WebElement sayisalLotoButton;
+
+    @FindBy(xpath = "//div[4]//div[1]//button[1]")
+    private WebElement superLotoButton;
+
+    @FindBy(xpath = "//div[5]//div[1]//button[1]")
+    private WebElement sansTopuButton;
+
+    @FindBy(xpath = "//div[6]//div[1]//button[1]")
+    private WebElement onNumaraButton;
+
+    @FindBy(xpath = "//option[@value=1 and contains(text(),'Çekiliş Bekleyen')]")
+    private WebElement lotteryPendingOption;
+
+    @FindBy(xpath = "//option[@value=3 and contains(text(),'Kaybeden')]")
+    private WebElement lotteryLoserOption;
+
+    @FindBy(xpath = "//option[@value=4 and contains(text(),'Kazanan')]")
+    private WebElement lotteryWinnerOption;
+
+    @FindBy(xpath = "//*[@id=\"periodFilter\"]/option[1]")
+    private WebElement threeDayOption;
+
+    @FindBy(xpath = "//*[@id=\"periodFilter\"]/option[2]")
+    private WebElement sevenDayOption;
+
+    @FindBy(xpath = "//*[@id=\"periodFilter\"]/option[3]")
+    private WebElement fifteenDayOption;
+
+    @FindBy(xpath = "//*[@id=\"periodFilter\"]/option[4]")
+    private WebElement oneMonthOption;
+
+    @FindBy(xpath = "//*[@id=\"periodFilter\"]/option[5]")
+    private WebElement twoMonthOption;
+
+    @FindBy(xpath = "//*[@id=\"periodFilter\"]/option[6]")
+    private WebElement threeMonthOption;
+
+    @FindBy(xpath = "//div[@class='msg-head show']")
+    private WebElement ticketWinText;
+
+    @FindBy(xpath = "//div[@class='stato-label show']")
+    private WebElement ticketText;
+
+    @FindBy(xpath = "(//img[contains(@src, 'sayisal-loto')])[2]")
+    private WebElement sayisalLotoLogo;
+
+    @FindBy(xpath = "(//img[contains(@src, 'super-loto')])[2]")
+    private WebElement superLotoLogo;
+
+    @FindBy(xpath = "(//img[contains(@src, 'on-numara')])[1]")
+    private WebElement onNumaraLogo;
+
+    @FindBy(xpath = "(//img[contains(@src, '-millipiyango')])[1]")
+    private WebElement milliPiyangoLogo;
+
+    @FindBy(xpath = "(//img[contains(@src, 'sans-topu')])[2]")
+    private WebElement sansTopuLogo;
 
     public MyTicketsPage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(EXPLICIT_WAIT));
         this.elementHelper = new ElementHelper(driver);
-        this.filterHelper = new FilterHelper(driver);
         PageFactory.initElements(driver, this);
     }
 
@@ -43,9 +102,9 @@ public class MyTicketsPage {
     }
 
     public void filterBySelecting(String game, String status, String dateRange) {
-        WebElement gameOption = filterHelper.getGameOption(driver, game);
-        WebElement statusOption = filterHelper.getStatusOption(driver, status);
-        WebElement dateOption = filterHelper.getDateOption(driver, dateRange);
+        WebElement gameOption = getGameOption(game);
+        WebElement statusOption = getStatusOption(status);
+        WebElement dateOption = getDateOption(dateRange);
 
         elementHelper.click(gameOption);
         elementHelper.click(statusOption);
@@ -56,20 +115,71 @@ public class MyTicketsPage {
         elementHelper.click(detailButton);
     }
 
-    public void validateTicketDetail(String expectedMessage,String status) {
-        WebElement validationElement = filterHelper.getValidationElement(driver, status);
+    public void validateTicketDetail(String expectedMessage, String status) {
+        WebElement validationElement = getValidationElement(status);
         elementHelper.checkVisible(validationElement);
         Assert.assertEquals(validationElement.getText(), expectedMessage);
     }
 
     public void selectGameFilter(String game) {
-        WebElement gameOption = filterHelper.getGameOption(driver, game);
+        WebElement gameOption = getGameOption(game);
         elementHelper.click(gameOption);
     }
 
     public void confirmFilter(String drawName) {
-        WebElement logoElement = filterHelper.getLogoElement(driver,drawName);
+        WebElement logoElement = getLogoElement(drawName);
         String actualSrc = logoElement.getAttribute("src");
         Assert.assertTrue(actualSrc.contains(drawName));
+    }
+
+    private WebElement getGameOption(String game) {
+        switch (game) {
+            case "On Numara": return onNumaraButton;
+            case "Şans Topu": return sansTopuButton;
+            case "Sayisal Loto": return sayisalLotoButton;
+            case "Milli Piyango": return milliPiyangoButton;
+            case "Süper Loto": return superLotoButton;
+            default: throw new IllegalArgumentException("Geçersiz oyun: " + game);
+        }
+    }
+
+    private WebElement getStatusOption(String status) {
+        switch (status) {
+            case "Çekiliş Bekleyen": return lotteryPendingOption;
+            case "Kazanan": return lotteryWinnerOption;
+            case "Kaybeden": return lotteryLoserOption;
+            default: throw new IllegalArgumentException("Geçersiz durum: " + status);
+        }
+    }
+
+    private WebElement getDateOption(String dateRange) {
+        switch (dateRange) {
+            case "3 gün": return threeDayOption;
+            case "7 gün": return sevenDayOption;
+            case "1 ay": return oneMonthOption;
+            case "2 ay": return twoMonthOption;
+            case "3 ay": return threeMonthOption;
+            default: throw new IllegalArgumentException("Geçersiz tarih aralığı: " + dateRange);
+        }
+    }
+
+    private WebElement getValidationElement(String status) {
+        switch (status) {
+            case "Kazanan": return ticketWinText;
+            case "Çekiliş Bekleyen":
+            case "Kazanamadın": return ticketText;
+            default: throw new IllegalArgumentException("Geçersiz durum: " + status);
+        }
+    }
+
+    private WebElement getLogoElement(String drawName) {
+        switch (drawName) {
+            case "sayisal-loto": return sayisalLotoLogo;
+            case "super-loto": return superLotoLogo;
+            case "on-numara": return onNumaraLogo;
+            case "millipiyango": return milliPiyangoLogo;
+            case "sans-topu": return sansTopuLogo;
+            default: throw new IllegalArgumentException("Geçersiz çekiliş adı: " + drawName);
+        }
     }
 }
